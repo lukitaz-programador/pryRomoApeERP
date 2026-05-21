06fd1b2ec1e32fd02e815e893717152038dcd554
lukitaz-programador <lucasromo.personal@gmail.com>
2026-05-21 09:05:23 -0300

    creacion de la group de redes

--- pryRomoApeERP/frmAplicacion.Designer.cs
+++ pryRomoApeERP/frmAplicacion.Designer.cs
@@ -29,24 +29,37 @@
         private void InitializeComponent()
         {
             this.grpDomicilio = new System.Windows.Forms.GroupBox();
-            this.grpDatosPer = new System.Windows.Forms.GroupBox();
-            this.txtNombre = new System.Windows.Forms.TextBox();
-            this.txtApellido = new System.Windows.Forms.TextBox();
-            this.lblDNI = new System.Windows.Forms.Label();
-            this.lblLocalidad = new System.Windows.Forms.Label();
-            this.lblProvincia = new System.Windows.Forms.Label();
-            this.cmbProvincia = new System.Windows.Forms.ComboBox();
-            this.cmbLocalidad = new System.Windows.Forms.ComboBox();
             this.txtGeo = new System.Windows.Forms.TextBox();
             this.txtDireccion = new System.Windows.Forms.TextBox();
             this.lblGeo = new System.Windows.Forms.Label();
             this.lblDireccion = new System.Windows.Forms.Label();
+            this.cmbLocalidad = new System.Windows.Forms.ComboBox();
+            this.cmbProvincia = new System.Windows.Forms.ComboBox();
+            this.lblProvincia = new System.Windows.Forms.Label();
+            this.lblLocalidad = new System.Windows.Forms.Label();
+            this.grpDatosPer = new System.Windows.Forms.GroupBox();
+            this.mskDNI = new System.Windows.Forms.MaskedTextBox();
             this.lblNombre = new System.Windows.Forms.Label();
             this.lblApellido = new System.Windows.Forms.Label();
-            this.mskDNI = new System.Windows.Forms.MaskedTextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.lblDNI = new System.Windows.Forms.Label();
             this.btnGuardarPer = new System.Windows.Forms.Button();
+            this.grpContacto = new System.Windows.Forms.GroupBox();
+            this.grpRedes = new System.Windows.Forms.GroupBox();
+            this.lblTikTok = new System.Windows.Forms.Label();
+            this.lblTelegram = new System.Windows.Forms.Label();
+            this.lblX = new System.Windows.Forms.Label();
+            this.lblInstagram = new System.Windows.Forms.Label();
+            this.lblFacebook = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblMail = new System.Windows.Forms.Label();
             this.grpDomicilio.SuspendLayout();
             this.grpDatosPer.SuspendLayout();
+            this.grpContacto.SuspendLayout();
+            this.grpRedes.SuspendLayout();
             this.SuspendLayout();
             // 
             // grpDomicilio
@@ -65,81 +78,6 @@
             this.grpDomicilio.TabIndex = 6;
             this.grpDomicilio.TabStop = false;
             this.grpDomicilio.Text = "Domicilio";
-            // 
-            // grpDatosPer
-            // 
-            this.grpDatosPer.Controls.Add(this.mskDNI);
-            this.grpDatosPer.Controls.Add(this.lblNombre);
-            this.grpDatosPer.Controls.Add(this.lblApellido);
-            this.grpDatosPer.Controls.Add(this.txtNombre);
-            this.grpDatosPer.Controls.Add(this.txtApellido);
-            this.grpDatosPer.Controls.Add(this.lblDNI);
-            this.grpDatosPer.Location = new System.Drawing.Point(16, 12);
-            this.grpDatosPer.Name = "grpDatosPer";
-            this.grpDatosPer.Size = new System.Drawing.Size(311, 156);
-            this.grpDatosPer.TabIndex = 7;
-            this.grpDatosPer.TabStop = false;
-            this.grpDatosPer.Text = "Datos Personales";
-            // 
-            // txtNombre
-            // 
-            this.txtNombre.Location = new System.Drawing.Point(97, 70);
-            this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(196, 20);
-            this.txtNombre.TabIndex = 9;
-            // 
-            // txtApellido
-            // 
-            this.txtApellido.Location = new System.Drawing.Point(97, 31);
-            this.txtApellido.Name = "txtApellido";
-            this.txtApellido.Size = new System.Drawing.Size(196, 20);
-            this.txtApellido.TabIndex = 7;
-            // 
-            // lblDNI
-            // 
-            this.lblDNI.AutoSize = true;
-            this.lblDNI.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
-            this.lblDNI.Location = new System.Drawing.Point(13, 111);
-            this.lblDNI.Name = "lblDNI";
-            this.lblDNI.Size = new System.Drawing.Size(33, 16);
-            this.lblDNI.TabIndex = 6;
-            this.lblDNI.Text = "DNI:";
-            // 
-            // lblLocalidad
-            // 
-            this.lblLocalidad.AutoSize = true;
-            this.lblLocalidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
-            this.lblLocalidad.Location = new System.Drawing.Point(12, 75);
-            this.lblLocalidad.Name = "lblLocalidad";
-            this.lblLocalidad.Size = new System.Drawing.Size(70, 16);
-            this.lblLocalidad.TabIndex = 13;
-            this.lblLocalidad.Text = "Localidad:";
-            // 
-            // lblProvincia
-            // 
-            this.lblProvincia.AutoSize = true;
-            this.lblProvincia.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
-            this.lblProvincia.Location = new System.Drawing.Point(12, 34);
-            this.lblProvincia.Name = "lblProvincia";
-            this.lblProvincia.Size = new System.Drawing.Size(66, 16);
-            this.lblProvincia.TabIndex = 14;
-            this.lblProvincia.Text = "Provincia:";
-            // 
-            // cmbProvincia
-            // 
-            this.cmbProvincia.FormattingEnabled = true;
-            this.cmbProvincia.Location = new System.Drawing.Point(97, 29);
-            this.cmbProvincia.Name = "cmbProvincia";
-            this.cmbProvincia.Size = new System.Drawing.Size(196, 21);
-            this.cmbProvincia.TabIndex = 17;
-            // 
-            // cmbLocalidad
-            // 
-            this.cmbLocalidad.FormattingEnabled = true;
-            this.cmbLocalidad.Location = new System.Drawing.Point(97, 70);
-            this.cmbLocalidad.Name = "cmbLocalidad";
-            this.cmbLocalidad.Size = new System.Drawing.Size(196, 21);
-            this.cmbLocalidad.TabIndex = 18;
             // 
             // txtGeo
             // 
@@ -175,6 +113,112 @@
             this.lblDireccion.TabIndex = 19;
             this.lblDireccion.Text = "Dirección:";
             // 
+            // cmbLocalidad
+            // 
+            this.cmbLocalidad.FormattingEnabled = true;
+            this.cmbLocalidad.Location = new System.Drawing.Point(97, 70);
+            this.cmbLocalidad.Name = "cmbLocalidad";
+            this.cmbLocalidad.Size = new System.Drawing.Size(196, 21);
+            this.cmbLocalidad.TabIndex = 18;
+            // 
+            // cmbProvincia
+            // 
+            this.cmbProvincia.FormattingEnabled = true;
+            this.cmbProvincia.Items.AddRange(new object[] {
+            "Buenos Aires",
+            "",
+            "Catamarca",
+            "",
+            "Chaco",
+            "",
+            "Chubut",
+            "",
+            "Córdoba",
+            "",
+            "Corrientes",
+            "",
+            "Entre Ríos",
+            "",
+            "Formosa",
+            "",
+            "Jujuy",
+            "",
+            "La Pampa",
+            "",
+            "La Rioja",
+            "",
+            "Mendoza",
+            "",
+            "Misiones",
+            "",
+            "Neuquén",
+            "",
+            "Río Negro",
+            "",
+            "Salta",
+            "",
+            "San Juan",
+            "",
+            "San Luis",
+            "",
+            "Santa Cruz",
+            "",
+            "Santa Fe",
+            "",
+            "Santiago del Estero",
+            "",
+            "Tierra del Fuego, Antártida e Islas del Atlántico Sur",
+            "",
+            "Tucumán"});
+            this.cmbProvincia.Location = new System.Drawing.Point(97, 29);
+            this.cmbProvincia.Name = "cmbProvincia";
+            this.cmbProvincia.Size = new System.Drawing.Size(196, 21);
+            this.cmbProvincia.TabIndex = 17;
+            // 
+            // lblProvincia
+            // 
+            this.lblProvincia.AutoSize = true;
+            this.lblProvincia.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.lblProvincia.Location = new System.Drawing.Point(12, 34);
+            this.lblProvincia.Name = "lblProvincia";
+            this.lblProvincia.Size = new System.Drawing.Size(66, 16);
+            this.lblProvincia.TabIndex = 14;
+            this.lblProvincia.Text = "Provincia:";
+            // 
+            // lblLocalidad
+            // 
+            this.lblLocalidad.AutoSize = true;
+            this.lblLocalidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.lblLocalidad.Location = new System.Drawing.Point(12, 75);
+            this.lblLocalidad.Name = "lblLocalidad";
+            this.lblLocalidad.Size = new System.Drawing.Size(70, 16);
+            this.lblLocalidad.TabIndex = 13;
+            this.lblLocalidad.Text = "Localidad:";
+            // 
+            // grpDatosPer
+            // 
+            this.grpDatosPer.Controls.Add(this.mskDNI);
+            this.grpDatosPer.Controls.Add(this.lblNombre);
+            this.grpDatosPer.Controls.Add(this.lblApellido);
+            this.grpDatosPer.Controls.Add(this.txtNombre);
+            this.grpDatosPer.Controls.Add(this.txtApellido);
+            this.grpDatosPer.Controls.Add(this.lblDNI);
+            this.grpDatosPer.Location = new System.Drawing.Point(16, 12);
+            this.grpDatosPer.Name = "grpDatosPer";
+            this.grpDatosPer.Size = new System.Drawing.Size(311, 156);
+            this.grpDatosPer.TabIndex = 7;
+            this.grpDatosPer.TabStop = false;
+            this.grpDatosPer.Text = "Datos Personales";
+            // 
+            // mskDNI
+            // 
+            this.mskDNI.Location = new System.Drawing.Point(97, 113);
+            this.mskDNI.Mask = "99999999";
+            this.mskDNI.Name = "mskDNI";
+            this.mskDNI.Size = new System.Drawing.Size(65, 20);
+            this.mskDNI.TabIndex = 14;
+            this.mskDNI.ValidatingType = typeof(int);
+            // 
             // lblNombre
             // 
             this.lblNombre.AutoSize = true;
@@ -195,29 +239,157 @@
             this.lblApellido.TabIndex = 12;
             this.lblApellido.Text = "Apellido:";
             // 
-            // mskDNI
+            // txtNombre
             // 
-            this.mskDNI.Location = new System.Drawing.Point(97, 113);
-            this.mskDNI.Mask = "99999999";
-            this.mskDNI.Name = "mskDNI";
-            this.mskDNI.Size = new System.Drawing.Size(65, 20);
-            this.mskDNI.TabIndex = 14;
-            this.mskDNI.ValidatingType = typeof(int);
+            this.txtNombre.Location = new System.Drawing.Point(97, 70);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(196, 20);
+            this.txtNombre.TabIndex = 9;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(97, 31);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(196, 20);
+            this.txtApellido.TabIndex = 7;
+            // 
+            // lblDNI
+            // 
+            this.lblDNI.AutoSize = true;
+            this.lblDNI.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.lblDNI.Location = new System.Drawing.Point(13, 111);
+            this.lblDNI.Name = "lblDNI";
+            this.lblDNI.Size = new System.Drawing.Size(33, 16);
+            this.lblDNI.TabIndex = 6;
+            this.lblDNI.Text = "DNI:";
             // 
             // btnGuardarPer
             // 
-            this.btnGuardarPer.Location = new System.Drawing.Point(233, 369);
+            this.btnGuardarPer.Location = new System.Drawing.Point(550, 380);
             this.btnGuardarPer.Name = "btnGuardarPer";
             this.btnGuardarPer.Size = new System.Drawing.Size(94, 23);
             this.btnGuardarPer.TabIndex = 8;
             this.btnGuardarPer.Text = "Guardar Datos";
             this.btnGuardarPer.UseVisualStyleBackColor = true;
             // 
+            // grpContacto
+            // 
+            this.grpContacto.Controls.Add(this.grpRedes);
+            this.grpContacto.Controls.Add(this.textBox2);
+            this.grpContacto.Controls.Add(this.lblTelefono);
+            this.grpContacto.Controls.Add(this.textBox1);
+            this.grpContacto.Controls.Add(this.lblMail);
+            this.grpContacto.Location = new System.Drawing.Point(333, 12);
+            this.grpContacto.Name = "grpContacto";
+            this.grpContacto.Size = new System.Drawing.Size(311, 351);
+            this.grpContacto.TabIndex = 9;
+            this.grpContacto.TabStop = false;
+            this.grpContacto.Text = "Contacto";
+            // 
+            // grpRedes
+            // 
+            this.grpRedes.Controls.Add(this.lblTikTok);
+            this.grpRedes.Controls.Add(this.lblTelegram);
+            this.grpRedes.Controls.Add(this.lblX);
+            this.grpRedes.Controls.Add(this.lblInstagram);
+            this.grpRedes.Controls.Add(this.lblFacebook);
+            this.grpRedes.Location = new System.Drawing.Point(19, 103);
+            this.grpRedes.Name = "grpRedes";
+            this.grpRedes.Size = new System.Drawing.Size(269, 230);
+            this.grpRedes.TabIndex = 4;
+            this.grpRedes.TabStop = false;
+            this.grpRedes.Text = "Redes Sociales";
+            // 
+            // lblTikTok
+            // 
+            this.lblTikTok.AutoSize = true;
+            this.lblTikTok.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.lblTikTok.Location = new System.Drawing.Point(16, 140);
+            this.lblTikTok.Name = "lblTikTok";
+            this.lblTikTok.Size = new System.Drawing.Size(53, 16);
+            this.lblTikTok.TabIndex = 4;
+            this.lblTikTok.Text = "TikTok:";
+            // 
+            // lblTelegram
+            // 
+            this.lblTelegram.AutoSize = true;
+            this.lblTelegram.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.lblTelegram.Location = new System.Drawing.Point(16, 35);
+            this.lblTelegram.Name = "lblTelegram";
+            this.lblTelegram.Size = new System.Drawing.Size(69, 16);
+            this.lblTelegram.TabIndex = 3;
+            this.lblTelegram.Text = "Telegram:";
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.lblX.Location = new System.Drawing.Point(17, 70);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(65, 16);
+            this.lblX.TabIndex = 2;
+            this.lblX.Text = "X (Twiter):";
+            // 
+            // lblInstagram
+            // 
+            this.lblInstagram.AutoSize = true;
+            this.lblInstagram.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.lblInstagram.Location = new System.Drawing.Point(17, 104);
+            this.lblInstagram.Name = "lblInstagram";
+            this.lblInstagram.Size = new System.Drawing.Size(69, 16);
+            this.lblInstagram.TabIndex = 1;
+            this.lblInstagram.Text = "Instagram:";
+            // 
+            // lblFacebook
+            // 
+            this.lblFacebook.AutoSize = true;
+            this.lblFacebook.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.lblFacebook.Location = new System.Drawing.Point(17, 179);
+            this.lblFacebook.Name = "lblFacebook";
+            this.lblFacebook.Size = new System.Drawing.Size(72, 16);
+            this.lblFacebook.TabIndex = 0;
+            this.lblFacebook.Text = "Facebook:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(87, 65);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(201, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.lblTelefono.Location = new System.Drawing.Point(16, 69);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(64, 16);
+            this.lblTelefono.TabIndex = 2;
+            this.lblTelefono.Text = "Teléfono:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(201, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // lblMail
+            // 
+            this.lblMail.AutoSize = true;
+            this.lblMail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.lblMail.Location = new System.Drawing.Point(16, 30);
+            this.lblMail.Name = "lblMail";
+            this.lblMail.Size = new System.Drawing.Size(35, 16);
+            this.lblMail.TabIndex = 0;
+            this.lblMail.Text = "Mail:";
+            // 
             // frmPersonal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(339, 404);
+            this.ClientSize = new System.Drawing.Size(660, 415);
+            this.Controls.Add(this.grpContacto);
             this.Controls.Add(this.btnGuardarPer);
             this.Controls.Add(this.grpDatosPer);
             this.Controls.Add(this.grpDomicilio);
@@ -228,6 +400,10 @@
             this.grpDomicilio.PerformLayout();
             this.grpDatosPer.ResumeLayout(false);
             this.grpDatosPer.PerformLayout();
+            this.grpContacto.ResumeLayout(false);
+            this.grpContacto.PerformLayout();
+            this.grpRedes.ResumeLayout(false);
+            this.grpRedes.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -251,5 +427,16 @@
         private System.Windows.Forms.Label lblNombre;
         private System.Windows.Forms.Label lblApellido;
         private System.Windows.Forms.Button btnGuardarPer;
+        private System.Windows.Forms.GroupBox grpContacto;
+        private System.Windows.Forms.GroupBox grpRedes;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblMail;
+        private System.Windows.Forms.Label lblInstagram;
+        private System.Windows.Forms.Label lblFacebook;
+        private System.Windows.Forms.Label lblTikTok;
+        private System.Windows.Forms.Label lblTelegram;
+        private System.Windows.Forms.Label lblX;
     }
 }
